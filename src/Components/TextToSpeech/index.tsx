baa88f84055d81ed93188bba5f51e8b745fbf089
rohanchugh14 <rohanchugh14@gmail.com>
2024-03-25 20:51:08 -0500

    make files import dynamically

--- src/Components/TextToSpeech/index.tsx
+++ src/Components/TextToSpeech/index.tsx
@@ -1,12 +1,6 @@
-// @ts-nocheck
 
 import { useEffect, useMemo, useState } from 'react';
 import { Button } from '@chakra-ui/react'
-import keeper1 from './keeper1.mp3'
-import keeper2 from './keeper2.mp3'
-import keeper3 from './keeper3.mp3'
-import keeper4 from './keeper4.mp3'
-import keeper5 from './keeper5.mp3'
 
 type Props = {
   runOnClickPrev: (e: any) => void,
@@ -14,15 +8,15 @@ type Props = {
   iter: number,
 }
 const AudioPlayer = ({runOnClickPrev, runOnClickNext, iter}: Props) => {
-  const audioFiles = useMemo(() => [keeper1, keeper2, keeper3, keeper4, keeper5], [])
+  const lastFileIndex = 4;
   const [isPlaying, setIsPlaying] = useState(false);
   const audio = useMemo(() => {
-    return new Audio(audioFiles[iter]); 
-  }, [audioFiles, iter]);
+    return new Audio(`/Assets/keeper${iter+1}.mp3`); 
+  }, [iter]);
 
   useEffect(() => {
-    audio.src = audioFiles[iter]
-  }, [audio, audioFiles, iter])
+    audio.src = `/Assets/keeper${iter+1}.mp3`
+  }, [audio, iter])
   useEffect(() => {
     const func = async() => {
       isPlaying ? await audio.play() : audio.pause()
@@ -51,7 +45,7 @@ const AudioPlayer = ({runOnClickPrev, runOnClickNext, iter}: Props) => {
       <Button colorScheme='red' size='lg' onClick={togglePlay}>
         {isPlaying ? 'Pause' : 'Play'}
       </Button>
-      <Button style={{ marginLeft: '5px' }} colorScheme='teal' size='lg' isDisabled={iter===audioFiles.length-1} onClick={(e: any) => {runOnClickNext(e)}}>
+      <Button style={{ marginLeft: '5px' }} colorScheme='teal' size='lg' isDisabled={iter===lastFileIndex} onClick={(e: any) => {runOnClickNext(e)}}>
         Next Nugget
       </Button>
     </div>
